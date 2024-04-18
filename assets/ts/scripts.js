e21254ba24912741ff5623e3ef56b24f048d0df4
Laurin Sawellion <laurin.sawellion@mni.thm.de>
2024-04-18 02:40:07 +0200

    Added the server sided data operations for: Create User, Render Users and delete User

--- assets/ts/scripts.js
+++ assets/ts/scripts.js
@@ -1,130 +1,196 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 // Definiert die Benutzerklasse mit den erforderlichen Attributen
 var UserObject = /** @class */ (function () {
-    function UserObject(firstName, lastName, email, password) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
+    function UserObject(firstname, lastname, mail, password) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.mail = mail;
         this.password = password;
     }
     return UserObject;
 }());
-// Initialisiert ein leeres Array zur Speicherung von User objekten
-var users = [];
 document.querySelector("#formCreate").addEventListener("submit", function (event) {
     event.preventDefault();
-    createUserObject();
+    addUser();
 });
-function createUserObjectRandom() {
-    for (var i = 0; i < 10; i++) {
-        var randomUser = createRandomUser();
-        scrollDown();
-        users.push(randomUser);
-    }
-    // Display function aufrufen
-    displayUserList();
-}
-function createRandomUser() {
-    // Erzeugt einen zufälligen Benutzer
-    var randomString = function () { return Math.random().toString(36).substring(7); };
-    return new UserObject("FirstName_" + randomString(), "LastName_" + randomString(), randomString() + "@example.com", "Password_" + randomString());
-}
-function createUserObject() {
-    var firstNameInput = document.getElementById("firstName");
-    var lastNameInput = document.getElementById("lastName");
-    var emailInput = document.getElementById("email");
-    var passwordInput = document.getElementById("password");
-    var firstName = firstNameInput.value;
-    var lastName = lastNameInput.value;
-    var email = emailInput.value;
-    var password = passwordInput.value;
-    // Trimmen der Werte
-    var trimmedFirstName = firstName.trim();
-    var trimmedLastName = lastName.trim();
-    var trimmedEmail = email.trim();
-    var trimmedPassword = password.trim();
-    if (trimmedFirstName && trimmedLastName && trimmedEmail && trimmedPassword) {
-        // Überprüfung, ob die Email im Array bereits existiert --> loopt durch jedes Array Element durch (for of loop)
-        for (var _i = 0, users_1 = users; _i < users_1.length; _i++) {
-            var existingUser = users_1[_i];
-            if (existingUser.email === trimmedEmail) {
-                alert("Die E-Mail Adresse " + trimmedEmail + " wird bereits benutzt. Bitte benutze eine andere Mail Adresse.");
-                // Beendet die Funktion wenn die Email bereits existiert
-                return;
+function addUser() {
+    return __awaiter(this, void 0, void 0, function () {
+        var firstNameInput, lastNameInput, emailInput, passwordInput, firstname, lastname, mail, password, response;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    firstNameInput = document.getElementById("firstName");
+                    lastNameInput = document.getElementById("lastName");
+                    emailInput = document.getElementById("email");
+                    passwordInput = document.getElementById("password");
+                    firstname = firstNameInput.value.trim();
+                    lastname = lastNameInput.value.trim();
+                    mail = emailInput.value.trim();
+                    password = passwordInput.value.trim();
+                    return [4 /*yield*/, fetch("https://userman.thuermer.red/api/users", {
+                            method: "POST",
+                            body: JSON.stringify({
+                                firstname: firstname,
+                                lastname: lastname,
+                                mail: mail,
+                                password: password
+                            }),
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            credentials: "include"
+                        })];
+                case 1:
+                    response = _a.sent();
+                    if (!response.ok) return [3 /*break*/, 3];
+                    firstNameInput.value = "";
+                    lastNameInput.value = "";
+                    emailInput.value = "";
+                    passwordInput.value = "";
+                    return [4 /*yield*/, renderUserList()];
+                case 2:
+                    _a.sent();
+                    scrollDown();
+                    return [3 /*break*/, 4];
+                case 3:
+                    console.log("Error: Response is not OK", response.statusText);
+                    _a.label = 4;
+                case 4: return [2 /*return*/];
             }
-        }
-        // Wenn die Email noch nicht benutzt wird, wird der Nutzer angelegt
-        var newUser = new UserObject(trimmedFirstName, trimmedLastName, trimmedEmail, trimmedPassword);
-        users.push(newUser);
-        // Formular leeren
-        firstNameInput.value = "";
-        lastNameInput.value = "";
-        emailInput.value = "";
-        passwordInput.value = "";
-        // Nutzer anzeigen
-        displayUserList();
-        scrollDown();
-    }
-    else {
-        alert("Bitte alle Eingabefelder ausfüllen, um den Nutzer zu erstellen.");
-    }
-}
-var userSelectIdx = null;
-function displayUserList() {
-    var tableBody = document.getElementById("userTableBody");
-    console.log(users);
-    if (tableBody) {
-        tableBody.innerHTML = "";
-        users.forEach(function (user, index) {
-            var row = tableBody.insertRow();
-            var emailCell = row.insertCell(0);
-            emailCell.textContent = user.email;
-            emailCell.setAttribute("data-label", "E-Mail");
-            var lastNameCell = row.insertCell(1);
-            lastNameCell.textContent = user.lastName;
-            lastNameCell.setAttribute("data-label", "Nachname");
-            var firstNameCell = row.insertCell(2);
-            firstNameCell.textContent = user.firstName;
-            firstNameCell.setAttribute("data-label", "Vorname");
-            // Füge Editier- und Lösch-Buttons hinzu
-            var actionsCell = row.insertCell(3);
-            var editButton = document.createElement("button");
-            editButton.className = "btn btn-warning m-3 bi bi-pen";
-            editButton.addEventListener("click", function () { return editUser(index); });
-            actionsCell.appendChild(editButton);
-            var deleteButton = document.createElement("button");
-            deleteButton.className = "btn btn-danger m-3 bi bi-trash";
-            deleteButton.addEventListener("click", function () { return deleteUser(index); });
-            actionsCell.appendChild(deleteButton);
         });
-    }
+    });
 }
-function editUser(index) {
-    // Setze den Index des bearbeiteten Benutzers
-    userSelectIdx = index;
-    console.log(userSelectIdx);
-    // die Eingabefelder für die Bearbeitung
-    var user = users[index];
-    var editFirstName = document.getElementById("editFirstName");
-    var editLastName = document.getElementById("editLastName");
-    var editEmail = document.getElementById("editEmail");
-    // Überprüfe, ob die Eingabefelder vorhanden sind
-    if (editFirstName && editLastName && editEmail) {
-        // setzt Userdaten in die Inputfelder
-        editFirstName.value = user.firstName;
-        editLastName.value = user.lastName;
-        editEmail.value = user.email;
-        // Öffnet das Bootstrap 5 Modal für die Bearbeitung
-        var editModal = new bootstrap.Modal(document.getElementById("editModal"));
-        editModal.show();
-    }
+function renderUserList() {
+    return __awaiter(this, void 0, void 0, function () {
+        var tableBody, response, users;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    tableBody = document.getElementById("userTableBody");
+                    return [4 /*yield*/, fetch("https://userman.thuermer.red/api/users", {
+                            credentials: "include"
+                        })];
+                case 1:
+                    response = _a.sent();
+                    if (!(response === null || response === void 0 ? void 0 : response.ok)) return [3 /*break*/, 3];
+                    return [4 /*yield*/, response.json()];
+                case 2:
+                    users = _a.sent();
+                    if (tableBody) {
+                        tableBody.innerHTML = "";
+                        users.forEach(function (user) {
+                            var row = tableBody.insertRow();
+                            var emailCell = row.insertCell(0);
+                            emailCell.textContent = user.mail;
+                            emailCell.setAttribute("data-label", "E-Mail");
+                            var lastNameCell = row.insertCell(1);
+                            lastNameCell.textContent = user.lastname;
+                            lastNameCell.setAttribute("data-label", "Nachname");
+                            var firstNameCell = row.insertCell(2);
+                            firstNameCell.textContent = user.firstname;
+                            firstNameCell.setAttribute("data-label", "Vorname");
+                            // Füge Editier- und Lösch-Buttons hinzu
+                            var actionsCell = row.insertCell(3);
+                            var editButton = document.createElement("button");
+                            editButton.className = "btn btn-warning m-3 bi bi-pen";
+                            editButton.addEventListener("click", function () { return editUserCloud(user.id); });
+                            console.log(user.id);
+                            actionsCell.appendChild(editButton);
+                            var deleteButton = document.createElement("button");
+                            deleteButton.className = "btn btn-danger m-3 bi bi-trash";
+                            deleteButton.addEventListener("click", function () { return deleteUserCloud(user.id); });
+                            actionsCell.appendChild(deleteButton);
+                        });
+                    }
+                    return [3 /*break*/, 4];
+                case 3:
+                    console.log("Error: Response is not OK", response.statusText);
+                    _a.label = 4;
+                case 4: return [2 /*return*/];
+            }
+        });
+    });
+}
+function editUserCloud(id) {
+    return __awaiter(this, void 0, void 0, function () {
+        var response, editUser, editFirstName, editLastName, editEmail, editModal;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, fetch("https://userman.thuermer.red/api/users/".concat(id), {
+                        credentials: "include"
+                    })];
+                case 1:
+                    response = _a.sent();
+                    if (!(response === null || response === void 0 ? void 0 : response.ok)) return [3 /*break*/, 4];
+                    return [4 /*yield*/, response.json()];
+                case 2:
+                    editUser = _a.sent();
+                    console.log(editUser);
+                    editFirstName = document.getElementById("editFirstName");
+                    editLastName = document.getElementById("editLastName");
+                    editEmail = document.getElementById("editEmail");
+                    return [4 /*yield*/, ];
+                case 3:
+                    _a.sent();
+                    if (editFirstName && editLastName && editEmail) {
+                        // setzt Userdaten in die Inputfelder
+                        editFirstName.value = editUser.firstname;
+                        editLastName.value = editUser.lastname;
+                        editEmail.value = editUser.mail;
+                        editModal = new bootstrap.Modal(document.getElementById("editModal"));
+                        editModal.show();
+                    }
+                    return [3 /*break*/, 5];
+                case 4:
+                    console.log("Error: Response is not OK", response.statusText);
+                    _a.label = 5;
+                case 5: return [2 /*return*/];
+            }
+        });
+    });
 }
 function updateUser() {
     console.log(userSelectIdx);
     // Überprüft, ob ein User bearbeitet wird
     if (userSelectIdx !== null) {
         // Input Felder für Bearbeitung
-        var editFirstNameInput = document.getElementById('editFirstName');
-        var editLastNameInput = document.getElementById('editLastName');
+        var editFirstNameInput = document.getElementById("editFirstName");
+        var editLastNameInput = document.getElementById("editLastName");
         // Trimmen der Werte
         var editFirstName = editFirstNameInput.value.trim();
         var editLastName = editLastNameInput.value.trim();
@@ -134,7 +200,7 @@ function updateUser() {
             users[userSelectIdx].firstName = editFirstName;
             users[userSelectIdx].lastName = editLastName;
             // Versteckt das Modal nach Bearbeitung
-            var editModal = new bootstrap.Modal(document.getElementById('editModal'));
+            var editModal = new bootstrap.Modal(document.getElementById("editModal"));
             editModal.hide();
             userSelectIdx = null;
             displayUserList();
@@ -144,16 +210,35 @@ function updateUser() {
         }
     }
 }
-function deleteUser(index) {
-    var result = window.confirm("Möchten Sie das Element wirklich löschen?");
-    if (result) {
-        users.splice(index, 1);
-        console.log("Nutzer Gelöscht!");
-    }
-    else {
-        // Element nicht löschen (Modal Hinzufuegen?)
-    }
-    displayUserList();
+function deleteUserCloud(id) {
+    return __awaiter(this, void 0, void 0, function () {
+        var result, response;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    result = window.confirm("Möchten Sie das Element wirklich löschen?");
+                    if (!result) return [3 /*break*/, 5];
+                    return [4 /*yield*/, fetch("https://userman.thuermer.red/api/users/".concat(id), {
+                            method: "DELETE",
+                            credentials: "include"
+                        })];
+                case 1:
+                    response = _a.sent();
+                    if (!(response === null || response === void 0 ? void 0 : response.ok)) return [3 /*break*/, 3];
+                    return [4 /*yield*/, renderUserList()];
+                case 2:
+                    _a.sent();
+                    return [3 /*break*/, 4];
+                case 3:
+                    console.log("Error: Response is not OK", response.statusText);
+                    _a.label = 4;
+                case 4:
+                    console.log("Nutzer Gelöscht!");
+                    _a.label = 5;
+                case 5: return [2 /*return*/];
+            }
+        });
+    });
 }
 function scrollDown() {
     //this.scroller.scrollToAnchor("targetGreen");
